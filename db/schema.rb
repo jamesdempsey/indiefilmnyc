efd5a3b89a12dae7f02e792d140740252f3db879
James Dempsey <james.e.dempsey@gmail.com>
2013-03-14 03:07:11 -0400

    created theaters table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130313192748) do
+ActiveRecord::Schema.define(:version => 20130314070444) do
 
   create_table "films", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "theaters", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
