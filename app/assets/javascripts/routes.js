4177cc6a5ab16a06bec2db32b2c8474bb50d417d
James Dempsey <james.e.dempsey@gmail.com>
2013-03-19 01:29:52 -0400

    removed comments

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -64,69 +64,13 @@ App.TheatersIndexRoute = App.TheatersRoute.extend({
   }
 });
 
-App.TheaterIndexRoute = App.TheatersRoute.extend({
-  model: function() {
-    return this.modelFor('theater');
+App.TheatersShowRoute = App.TheatersRoute.extend({
+  model: function(params) {
+    return App.Theater.find(params.theater_id);
   },
   setupController: function(controller, model) {
     this._super();
-    controller.set('theater', model);
+    controller.set('theaters', model);
+    controller.set('scoped_showing', App.Showing.find());
   }
 });
-
-//App.Router.reopen
-  //location: 'history'
-  //rootURL: '/'
-
-//App.Router.map (match) ->
-  //@resource 'films', ->
-    //@route 'show',
-      //path: '/:film_id'
-  //#@resource 'theaters',
-      //#path: '/:theater_id', ->
-      //#@route 'show',
-      //#@resource 'films', ->
-  //@resource 'theaters', ->
-    //@route 'show',
-      //path: '/:theater_id', ->
-        //@resource 'films', ->
-
-//App.IndexRoute = Ember.Route.extend
-  //setupController: (controller, model) ->
-    //@controllerFor('application').set('currentRoute', 'home')
-
-//App.FilmsRoute = Ember.Route.extend
-  //setupController: ->
-    //@controllerFor('application').set('currentRoute', 'films')
-
-//App.FilmsIndexRoute = App.FilmsRoute.extend
-  //model: ->
-    //App.Film.find()
-  //setupController: (controller, model) ->
-    //@_super()
-    //controller.set('films', model)
-
-//App.FilmsShowRoute = App.FilmsRoute.extend
-  //model: (params) ->
-    //App.Film.find(params.film_id)
-  //setupController: (controller, model) ->
-    //@_super()
-    //controller.set('films', model)
-
-//App.TheatersRoute = Ember.Route.extend
-  //setupController: ->
-    //@controllerFor('application').set('currentRoute', 'theaters')
-
-//App.TheatersIndexRoute = App.TheatersRoute.extend
-  //model: ->
-    //App.Theater.find()
-  //setupController: (controller, model) ->
-    //@_super()
-    //controller.set('theaters', model)
-
-//App.TheatersShowRoute = App.TheatersRoute.extend
-  //model: (params) ->
-    //App.Theater.find(params.theater_id)
-  //setupController: (controller, model) ->
-    //@_super()
-    //controller.set('theater', model)
