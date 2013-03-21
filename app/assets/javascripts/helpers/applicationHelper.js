0e016cf6b8553ad89e29df009d5426867650a23b
James Dempsey <james.e.dempsey@gmail.com>
2013-03-21 18:59:09 -0400

    helper method no longer needed for showings filtering

--- app/assets/javascripts/helpers/applicationHelper.js
+++
@@ -1,6 +0,0 @@
-Ember.Handlebars.registerBoundHelper('getTheaterShowtime', function(theater, showing) {
-  if (showing.get('theater_id') == theater.get('id')) {
-    escaped = Handlebars.Utils.escapeExpression(showing.get('showtime'));
-    return new Handlebars.SafeString('<li>' + escaped + '</li>');
-  }
-});
