ef2f6f6d028dd63cddd6d006f4f5d7b24757d1da
James Dempsey <james.e.dempsey@gmail.com>
2013-03-23 14:42:34 -0400

    sort by date for film show

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,6 +1,6 @@
 class Film < ActiveRecord::Base
   attr_accessible :description, :title
 
-  has_many :showings
+  has_many :showings, order: 'showtime ASC'
   has_many :theaters, through: :showings, uniq: true
 end
